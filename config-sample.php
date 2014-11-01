9161f553281c615203c8a7f990d945172a22ef79
Baguz A <eggoez@users.noreply.github.com>
2014-11-01 07:42:03 +0700

    Create config-sample.php

---
+++ config-sample.php
@@ -0,0 +1,9 @@
+<? $admin='admin';
+$pwd='pass';
+$email='your@mail.com';
+$title='Simple Memo';
+$slog='Just Another Stiky Notes';
+$post='3';//post per page
+$kord='7';//Timezone
+$postkom='5';//comment per page
+?>
