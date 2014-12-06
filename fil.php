32114846aab188426f6384924f2d2bfa99e884b1
Baguz A <eggoez@users.noreply.github.com>
2014-12-06 11:12:43 +0700

    Update fil.php

--- fil.php
+++ fil.php
@@ -5,26 +5,26 @@ if(session_is_registered('log')){
 switch($ad)
 {case ipx :
 if($ip!='')
-{$op=fopen("./x/ip.txt","a");
+{$op=fopen("./lib/ip.txt","a");
 fwrite($op,"$ip\r\n");
 fclose($op);
 echo "
 <div class=\"alert alert-dismissable alert-success\">
   <button type=\"button\" class=\"close\" data-dismiss=\"alert\">×</button>
-  <strong>Oke!</strong> $ip success added. <a onclick=\"history.back()\" class=\"alert-link\">Back</a>.
+  <strong>Oke!</strong> $ip telah ditambahkan. <a onclick=\"history.back()\" class=\"alert-link\">Kembali.</a>.
 </div>
 ";}
 else{}
 break;
 case spamx :
 if($spam!='')
-{$op=fopen("./x/spam.txt","a");
+{$op=fopen("./lib/spam.txt","a");
 fwrite($op,"$spam\r\n");
 fclose($op);
 echo "
 <div class=\"alert alert-dismissable alert-success\">
   <button type=\"button\" class=\"close\" data-dismiss=\"alert\">×</button>
-  <strong>Oke!</strong> add $spam to spam list successfully. <a onclick=\"history.back()\" class=\"alert-link\">Back</a>.
+  <strong>Oke!</strong> $spam berhasil ditambahkan ke daftar Spam!. <a onclick=\"history.back()\" class=\"alert-link\">Kembali.</a>.
 </div>
 ";}
 else{}
@@ -34,12 +34,12 @@ switch($fil){
 case ip : ?>
 <form action="<?=$PHP_SELF;?>?ad=ipx" method="post">
 <div class="form-group">
-  <label class="control-label">Input IP</label>
+  <label class="control-label">Masukkan IP</label>
   <div class="input-group">
     <span class="input-group-addon"><i class="fa fa-ban"></i></span>
     <input class="form-control" type="text" name="ip">
     <span class="input-group-btn">
-      <button class="btn btn-default" type="submit" Value="Tambahkan">Add</button>
+      <button class="btn btn-default" type="submit" Value="Tambahkan">Tambah</button>
     </span>
   </div>
 </form>
@@ -48,12 +48,12 @@ break;
 case spam : ?>
 <form action="<?=$PHP_SELF;?>?ad=spamx" method="post">
 <div class="form-group">
-  <label class="control-label">Add Spam</label>
+  <label class="control-label">Tambah Spam</label>
   <div class="input-group">
     <span class="input-group-addon"><i class="fa fa-times"></i></span>
     <input class="form-control" type="text" name="spam">
     <span class="input-group-btn">
-      <button class="btn btn-default" type="submit" Value="Tambahkan">Add</button>
+      <button class="btn btn-default" type="submit" Value="Tambahkan">Tambahkan</button>
     </span>
   </div>
 </form>
@@ -63,7 +63,7 @@ break;
 switch($show)
 {
 case ip :
-$data=file("./x/ip.txt");
+$data=file("./lib/ip.txt");
 $total=count($data);
 $post=15;
 if ($hal == "")
@@ -74,7 +74,7 @@ if ($second < 1) {$second = 1;}
 $hals = ($total / $post);
 if ($hal>1) $rew = "<a href=\"$PHP_SELF?show=ip&amp;hal=".($hal-1)."\">".($hal-1)."</a>";
 if ($hal<$hals) $next = "<a href=\"$PHP_SELF?show=ip&amp;hal=".($hal+1)."\">".($hal+1)."</a>";
-echo "<div class=\"panel panel-danger\">  <div class=\"panel-heading\">    <h3 class=\"panel-title\">List of Banned IPs</h3>  </div>
+echo "<div class=\"panel panel-danger\">  <div class=\"panel-heading\">    <h3 class=\"panel-title\">Daftar IP Banned</h3>  </div>
 ";
 for ($i = $first-1; $i >= $second-1; $i--) 
 {
@@ -82,11 +82,11 @@ $ii = $i;$ii++;
 echo $data[$i].' </br>';}
 echo "
 </div></div>
-<p><a class=\"btn btn-default\" href=\"$PHP_SELF?fil=ip\">Add</a></p></div>
+<p><a class=\"btn btn-default\" href=\"$PHP_SELF?fil=ip\">Tambah</a></p></div>
 <th><p>$rew  $next</p>";
 break;
 case spam :
-$data=file("./x/spam.txt");
+$data=file("./lib/spam.txt");
 $total=count($data);
 $post=15;
 if ($hal == "")
@@ -100,7 +100,7 @@ if ($hal<$hals) $next = "<a href=\"$PHP_SELF?show=spam&amp;hal=".($hal+1)."\">".
 echo "
 <div class=\"panel panel-warning\">
   <div class=\"panel-heading\">
-    <h3 class=\"panel-title\">List of Spam</h3>
+    <h3 class=\"panel-title\">Daftar Kata Spam</h3>
   </div>
   <div class=\"panel-body\">
 
@@ -113,7 +113,7 @@ echo $isi[0]."</br>";}
 echo "
   </div>
 </div>
-<p><a class=\"btn btn-default\" href=\"$PHP_SELF?fil=spam\">Add</a></p></div>
+<p><a class=\"btn btn-default\" href=\"$PHP_SELF?fil=spam\">Tambah</a></p></div>
 <p>$rew  $next</p>";
 break;
 }
