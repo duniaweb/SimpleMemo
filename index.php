bedfbab20cfcc81c56e52920f40d1f1598e6b484
Baguz A <eggoez@users.noreply.github.com>
2014-12-06 11:13:28 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,7 @@
 <?session_start();
-
 include"head.php";
 if($a=='login'){
 ?>
-
 <form action="aut.php" method="post">
 <div class="input-group">
     <span class="input-group-addon"><i class="fa fa-key"></i></span>
@@ -15,23 +13,21 @@ if($a=='login'){
 </form>
 <?
 exit();}
-
 if ($hal == ""){$hal = "1";}
-  $xfile=@file("arsip/art.txt");
-  $first = count($xfile) - ($post * ($hal - 1));
-  $second = count($xfile) - ($post * $hal) + 1;
+$xfile=@file("content/art.txt");
+$first = count($xfile) - ($post * ($hal - 1));
+$second = count($xfile) - ($post * $hal) + 1;
 if ($second < 1) {$second = 1;}
-  $hals = (count($xfile) / $post);
-if ($hal>1) $rew = "<a class=\"btn btn-primary btn-xs\" href=\"./?hal=".($hal-1)."\">« Previous</a>";
-if ($hal<$hals) $next = "<a class=\"btn btn-primary btn-xs\" href=\"./?hal=".($hal+1)."\">Next »</a>";
+$hals = (count($xfile) / $post);
+if ($hal>1) $rew = "<a class=\"btn btn-primary btn-xs\" href=\"./?hal=".($hal-1)."\">« Sebelumnya</a>";
+if ($hal<$hals) $next = "<a class=\"btn btn-primary btn-xs\" href=\"./?hal=".($hal+1)."\">Selanjutnya »</a>";
 for ($i = $first-1; $i >= $second-1; $i--) {
 $ii = $i;
 $ii++;
 $data = explode("|",$xfile[$i]);
-$fkom=file("./arsip/kom_$data[0].txt");
-print "
-<span class=\"label label-default\">
-<i class=\"fa fa-calendar-o\"></i> $data[1] <i class=\"fa fa-clock-o\"></i> $data[2]</span> <span class=\"label label-primary\"><i class=\"fa fa-comments-o\"></i> ".count($fkom)." comments.</span>
+$fkom=file("./content/kom_$data[0].txt");
+print "<div class=\"isi\">
+<span class=\"label label-default\"><i class=\"fa fa-calendar-o\"></i> $data[1] <i class=\"fa fa-clock-o\"></i> $data[2]</span> <span class=\"label label-primary\"><i class=\"fa fa-comments-o\"></i> ".count($fkom)." komentar.</span>
 <div class=\"list-group\">
   <a href=\"./art.php?id=$data[0]\" class=\"list-group-item\">
      <h4 class=\"list-group-item-heading\"><i class=\"fa fa-paperclip\"></i> ".ucfirst($data[4])."</h4>
@@ -40,7 +36,7 @@ print "
 ";
 if(session_is_registered('log')){
 echo "<a class=\"label label-primary\" href=\"./edit.php?id=$data[0]\">Edit</a>
-<a class=\"label label-danger\" href=\"./dels.php?del=entry&amp;id=$data[0]\">Delete</a>";}
+<a class=\"label label-danger\" href=\"./dels.php?del=entry&amp;id=$data[0]\">Hapus</a>";}
 echo "</div>";
 }
 print "<center>" . $rew."&nbsp;".$next."
