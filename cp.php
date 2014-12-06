c8557fba9de1d8e5775157a2f4357b437974ef27
Baguz A <eggoez@users.noreply.github.com>
2014-12-06 11:11:01 +0700

    Update cp.php

--- cp.php
+++ cp.php
@@ -1,10 +1,5 @@
 <?
-if(session_is_registered('log')){?><div class="btn-group btn-group-justified">  
-<a href="./nyar.php?x=artikel" class="btn btn-default">Write</a>  
-<a href="./fil.php?show=ip" class="btn btn-default">Block IP</a>  
-<a href="./fil.php?show=spam" class="btn btn-default">Antispam</a>
-</div>
-<a href="./aut.php?a=ot" class="btn btn-default btn-lg btn-block">Logout</a>
+if(session_is_registered('log')){?><div class="btn-group btn-group-justified">  <a href="./new.php?x=artikel" class="btn btn-default">Tulis</a>  <a href="./fil.php?show=ip" class="btn btn-default">Kelola IP</a>  <a href="./fil.php?show=spam" class="btn btn-default">Kelola Spam</a></div><a href="./aut.php?a=ot" class="btn btn-default btn-lg btn-block">Keluar</a>
 <?}
 else{?><div class="jumbotron"> 
 <?}?> 
