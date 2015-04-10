a2255d105c968393ba5d507487bd695a7579179b
Baguz Ach <eggoez@users.noreply.github.com>
2015-04-10 15:28:03 +0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
 <p><a href="./?a=login">&#169; <?=$title.'  '.gmdate("Y");?></a> </p></div>
 <div class="container theme-showcase" role="main">
+<hr />
 <p><small>Powered by <a href="https://github.com/eggoez/SimpleMemo" target="_new" title="Simple Memo">Simple Memo</a>.</small></p>
 </div>
 </div>
